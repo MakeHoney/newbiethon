f952f5127e27b485fe6efe480ba61465e300bb7b
MakeHoney <makehoney3@gmail.com>
2018-05-05 18:19:35 +0900

    default

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'question/index'
+  get 'main/index'
+  devise_for :users
+
+  resources :chatrooms do
+  	resource :chatroom_users
+  end
+  get '/chat' => 'chatrooms#index'
+  root to: 'main#index'
+  # scaffolding을 chatroom으로 해놓았기 때문에 root로 설정
+  
+
+end
